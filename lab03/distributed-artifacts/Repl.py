9011b812220ab2e3a95708865ee53f17b13f0d59
Pawel Konopka <p.konopka@avsystem.com>
2024-04-09 12:32:13 +0200

    Command parsing refactor

--- lab03/distributed-artifacts/Repl.py
+++ lab03/distributed-artifacts/Repl.py
@@ -9,34 +9,48 @@ from Document import Document
 
 
 class Command:
-    def __init__(self, name, usage, description) -> None:
+    def __init__(self, name: str, argument_count: int, usage: str, description: str) -> None:
         self.name = name
+        self.argument_count = argument_count
         self.usage = usage
         self.description = description
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Command):
+            return False
+        return (self.name == other.name and
+                self.argument_count == other.argument_count and
+                self.usage == other.usage and
+                self.description == other.description)
 
 class AvailableCommands:
     help = Command(
         name="help",
+        argument_count=0,
         usage="help",
         description="Display help",
     )
     upload = Command(
         name="upload",
+        argument_count=2,
         usage="upload [document name] [content]",
         description="Upload new document",
     )
     read = Command(
         name="read",
+        argument_count=1,
         usage="read [document name]",
         description="Read the content of an existing document",
     )
     status = Command(
         name="status",
+        argument_count=0,
         usage="status",
         description="Inspect cluster status",
     )
     exit = Command(
         name="exit",
+        argument_count=0,
         usage="exit",
         description="Shutdown the server",
     )
@@ -59,11 +73,11 @@ class Repl:
                 self.__handle_user_input(command, validated_arguments)
 
 
-    def __handle_user_input(self, command, validated_arguments) -> str:
-        if command == AvailableCommands.help.name:
+    def __handle_user_input(self, command: Command, validated_arguments: list[str]) -> str:
+        if command == AvailableCommands.help:
             print(self.__get_help_message(), end="\n\n")
         
-        elif command == AvailableCommands.upload.name:
+        elif command == AvailableCommands.upload:
             document_name, document_content = validated_arguments
             new_document = Document(document_name, document_content)
             error = ray.get(self.__client.save.remote(new_document))
@@ -73,7 +87,7 @@ class Repl:
             else:
                 print(f"Successfully uploaded document '{document_name}'")
     
-        elif command == AvailableCommands.read.name:
+        elif command == AvailableCommands.read:
             [document_name] = validated_arguments
             start_time = time()
             document, node_id, error = ray.get(self.__client.read.remote(document_name))
@@ -84,12 +98,12 @@ class Repl:
             else:
                 print(f"Document '{document_name}' successfully read from DataNode#{node_id} in {execution_time:.2f}s\nContent:\n" + Fore.BLUE + document.content + Style.RESET_ALL)
 
-        elif command == AvailableCommands.status.name:
+        elif command == AvailableCommands.status:
             name_node_status, data_nodes_statuses = ray.get(self.__client.get_status.remote())
             
             print(self.__parse_nodes_status(name_node_status, data_nodes_statuses), end="\n\n")
 
-        elif command == AvailableCommands.exit.name:
+        elif command == AvailableCommands.exit:
             print("Shutting down Distributed-Artifact system...")
             ray.shutdown()
             sys.exit()
@@ -101,13 +115,13 @@ class Repl:
     def __print_error(cls, error_message: str):
             sys.stderr.write(Fore.RED + f"ERROR: {error_message}\n" + Style.RESET_ALL)
 
-    def __parse_user_input(cls, raw_user_input: str) -> tuple[str, list[str], str]:
-        """Returns: `(command: str, validated_arguments: list[str], error: str)`"""
+    def __parse_user_input(cls, raw_user_input: str) -> tuple[Command, list[str], str]:
+        """Returns: `(command: Command, validated_arguments: list[str], error: str)`"""
         
         def unexpected_arguments_msg(command: Command, args: list[str]) -> str:
             return f"Command '{command.name}' received unexpected arguments: {args}\nUsage: {command.usage}"
         
-        def invalid_arguments_msg(command: Command, detailed_error: str) -> str:
+        def invalid_arguments_msg(command: Command, detailed_error: str = None) -> str:
             msg = f"Command '{command.name}' received invalid arguments"
             msg += f": {detailed_error}" if detailed_error is not None else "."
             msg += f"\nUsage: {command.usage}"
@@ -119,48 +133,22 @@ class Repl:
         if len(user_input) == 0:
             return None, [], None
         
-        command = user_input[0]
+        command_name = user_input[0]
         arguments = user_input[1:]
 
-        if command == AvailableCommands.help.name:
-            if len(arguments) != 0:
-                error = unexpected_arguments_msg(AvailableCommands.help, arguments)
-                return command, [], error
-            
-            return command, [], None
-        
-        if command == AvailableCommands.upload.name:
-            if len(arguments) != 2:
-                error = invalid_arguments_msg(AvailableCommands.exit)
-                return command, [], error
-            
-            document_name, content = arguments
-            return command, [document_name, content], None
-        
-        if command == AvailableCommands.read.name:
-            if len(arguments) != 1:
-                error = invalid_arguments_msg(AvailableCommands.read)
-                return command, [], error
-            
-            [document_name] = arguments
-            return command, [document_name], None
-        
-        if command == AvailableCommands.status.name:
-            if len(arguments) != 0:
-                error = unexpected_arguments_msg(AvailableCommands.status, arguments)
-                return command, [], error
-            
-            return command, [], None
+        matching_commands = [command for command in AvailableCommands.all if command.name == command_name]
+        assert len(matching_commands) <= 1, f"Internal error: More than 1 command matched name {command_name}"
 
-        if command == AvailableCommands.exit.name:
-            if len(arguments) != 0:
-                error = unexpected_arguments_msg(AvailableCommands.exit, arguments)
-                return command, [], error
-            
-            return command, [], None
+        if len(matching_commands) == 0:
+            error = f"Unknown command `{command_name}`. Write `{AvailableCommands.help.name}` for help"
+            return None, [], error
+
+        command = matching_commands[0]
+        if len(arguments) != command.argument_count:
+            error = unexpected_arguments_msg(command, arguments) if command.argument_count == 0 else invalid_arguments_msg(command)
+            return command, [], error
         
-        error = f"Unknown command `{command}`. Write `{AvailableCommands.help.name}` for help"
-        return None, [], error
+        return command, arguments, None
     
 
     def __get_help_message(cls) -> str:
@@ -185,9 +173,9 @@ class Repl:
         def parse_data_node_status(status: tuple[int, list[str]]):
             node_id, saved_document_names = status
 
-            return f"NameNode#{node_id} stores {len(saved_document_names)} documents:\n{sorted(saved_document_names)}"
+            return Fore.BLUE + f"DataNode#{node_id}" + Style.RESET_ALL + f" stores {len(saved_document_names)} documents:\n{sorted(saved_document_names)}"
 
-        name_node_msg = f"NameNode:\n{parse_name_node_status(name_node_status)}"
+        name_node_msg = Fore.BLUE + "NameNode" + Style.RESET_ALL + f":\n{parse_name_node_status(name_node_status)}"
 
         data_nodes_statuses_msgs = [parse_data_node_status(status) for status in sorted(data_nodes_statuses)]
         data_nodes_msg = "\n".join(data_nodes_statuses_msgs)
